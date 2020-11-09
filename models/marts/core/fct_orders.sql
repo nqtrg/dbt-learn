b483aa203f235c5f3aac2e4dc0163fc1c2602ffd
Ngu Truong <timexdanang@gmail.com>
2020-11-09 08:29:41 +0000

    update dim_customers

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,7 +1,30 @@
-select 
-    orders.order_id,
-    orders.customer_id,
-    payment.amount
-from {{ ref('stg_orders') }} as orders 
-left join {{ ref('stg_payment')}} as payment 
-    on orders.order_id = payment.orderid
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
+
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        orderid,
+        sum(case when status = 'success' then amount end) as amount
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments 
+        on orders.order_id = order_payments.orderid
+)
+
+select * from final
--- models/staging/jaffle_shop/stg_customer_spend.sql
+++
@@ -1,4 +0,0 @@
-select customer_id, 
-sum(amount) as lifetime_value
- from {{ ref('fct_orders' )}} as orders
- group by customer_id
